10849a1c389761b06ede4922f6a4235637a42e3e
farid <parlenor@mail.ru>
2015-05-13 19:26:51 +0300

    Update main.c Conflicts: 	main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     char first[255], last[255];
 	
     askname(first, last);
-	printf("World is salam, %s %s!\n");
+	printf("World is nicheqo tak, %s %s!\n");
     printf("Hello, %s %s!\n", first, last);
     return 0;
 }
